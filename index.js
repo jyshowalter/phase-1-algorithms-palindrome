60a5308b0373b1fd6008d4254218f0838a441275
Jason <jyshowalter@gmail.com>
2023-06-19 11:42:11 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+
+let reversedWord
+
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i = 0; i < word.length /2; i++) {
+    if (word[i] != word[word.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+check if first and last letter match 
+move inwards until complete
+return false if not a match
+return true if all letters match
 */
 
 /*
